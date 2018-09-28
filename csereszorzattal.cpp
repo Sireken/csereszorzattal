7b2561f97afc05a7f38b8133f0b03855a5b94c1a
Sireken <43463273+Sireken@users.noreply.github.com>
2018-09-28 16:13:33 +0200

    Add files via upload

---
+++ csereszorzattal.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	int a=0;
+	int b=0;
+
+	cout<<"Add meg a két változót!\n";
+	cin>>a;
+	cin>>b;
+	a=a*b;
+	b=a/b;
+	a=a/b;
+	cout<<a<<endl;
+	cout<<b<<endl;
+
+
+}
